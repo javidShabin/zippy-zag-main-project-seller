bb32084dc969b6bf4f5b0898491811a2bf47f562
javid shabin <javid.main.act@gmail.com>
2024-12-13 12:56:31 +0530

    create and set route for dashbord component

---
+++ src/pages/Dashbord.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dashbor = () => {
+  return <div>Dashbord</div>;
+};
+
+export default Dashbor;
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,10 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import SignupPage from "../pages/SignupPage";
 import SellerLayout from "../layout/SellerLayout";
+import Dashbor from "../pages/Dashbord";
+
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <SellerLayout />
+        element: <SellerLayout />,
+
+        children: [
+           {
+            path: "/",
+            element: <Dashbor />
+           }
+        ]
     }
 ])
