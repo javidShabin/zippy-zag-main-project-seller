2ddb5ddfeb7e58a8225842b216240ba0b1a7e0cf
javid shabin <javid.main.act@gmail.com>
2024-12-13 12:28:29 +0530

    create and config the seller layout

--- src/layout/SellerLayout.jsx
+++ src/layout/SellerLayout.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { axiosInstance } from "../config/axiosInstance";
+import { clearSeller, saveSeller } from "../redux/features/sellerSlice";
+import UnAuthSidebar from "../components/UnAuthSidebar";
+import AuthSidebar from "../components/AuthSeller/AuthSidebar";
 
 const SellerLayout = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { isSellerExist } = useSelector((state) => state.seller);
+  console.log(isSellerExist);
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const [loading, setLoading] = useState(true);
+  const checkSeller = async () => {
+    try {
+      await axiosInstance({
+        method: "GET",
+        url: "/seller/check-seller",
+      });
+      dispatch(saveSeller());
+    } catch (error) {
+      dispatch(clearSeller());
+      console.log(error);
+    } finally {
+      setLoading(false); // Set loading to false after checking user
+    }
+  };
+  useEffect(() => {
+    checkSeller();
+  }, [location.pathname]);
 
-export default SellerLayout
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <span className="loading loading-dots loading-lg bg-orange-400"></span>
+      </div>
+    );
+  }
+  return (
+    <div className="flex h-screen">
+      <div className="sticky top-0 w-64 bg-white shadow-lg">
+        {isSellerExist ? <AuthSidebar /> : <UnAuthSidebar />}
+      </div>
+      {/* Main Content Section */}
+      <div className="flex-1 bg-gray-100 p-4 overflow-y-auto">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default SellerLayout;
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import SignupPage from "../pages/SignupPage";
+import SellerLayout from "../layout/SellerLayout";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <SignupPage />
+        element: <SellerLayout />
     }
 ])
