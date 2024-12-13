4338155b2004cae78baeb366170a44c031eb52b3
javid shabin <javid.main.act@gmail.com>
2024-12-13 13:23:03 +0530

    make a change

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import SignupPage from "../pages/SignupPage";
 import SellerLayout from "../layout/SellerLayout";
 import Dashbor from "../pages/Dashbord";
+import LoginPage from "../pages/LoginPage";
 
 
 export const router = createBrowserRouter([
@@ -13,6 +14,14 @@ export const router = createBrowserRouter([
            {
             path: "/",
             element: <Dashbor />
+           },
+           {
+            path: "sign-up",
+            element: <SignupPage />
+           },
+           {
+            path: "log-in",
+            element: <LoginPage />
            }
         ]
     }
