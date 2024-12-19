392d3e6e1d157fe34154bc1705e0df1eaf22fecd
javid shabin <javid.main.act@gmail.com>
2024-12-19 14:10:18 +0530

    make add the orderlist

--- src/pages/Dashbord.jsx
+++ src/pages/Dashbord.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import RestaurantDetails from "./authSeller/RestaurantDetails";
+import OrderList from "./authSeller/OrderList";
 
 const Dashbord = () => {
   return (
     <>
     <RestaurantDetails />
+    <OrderList />
     </>
   )
 };
