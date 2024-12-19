604d60e14ed5a7607e1cf04ef79f59bf8928a14e
javid shabin <javid.main.act@gmail.com>
2024-12-19 17:22:36 +0530

    check the id have or not

--- src/components/AuthSeller/GetOrderDetails.jsx
+++ src/components/AuthSeller/GetOrderDetails.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const GetOrderDetails = () => {
+    const {orderId} = useParams()
+
+    console.log(orderId, "==id")
   return (
     <div>
-      
+      <h1>The id {orderId}</h1>
     </div>
   )
 }
