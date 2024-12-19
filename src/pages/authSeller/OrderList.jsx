8223bb1f9b5cfb84142d99c70ba97037831bc7c7
javid shabin <javid.main.act@gmail.com>
2024-12-19 14:37:20 +0530

    make aa change

--- src/pages/authSeller/OrderList.jsx
+++ src/pages/authSeller/OrderList.jsx
@@ -2,10 +2,25 @@ import React, { useEffect } from 'react'
 import { axiosInstance } from '../../config/axiosInstance'
 
 const OrderList = () => {
+  const [restaurantId, setRestaurantId] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axiosInstance.get("/seller/profile");
+        setRestaurantId(response.data.restaurant);
+        console.log(response, "==der")
+      } catch (err) {
+        console.error("Error fetching restaurant ID:", err);
+        
+      }
+    };
+    fetchData();
+  }, []);
+
     useEffect(()=>{
        const getOrderList = async () => {
         try {
-            const response = await axiosInstance.get("/payment/session-status")
+            const response = await axiosInstance.get(`/payment/orderBy-restaurant/${restaurantId}`)
             console.log(response, "==details")
         } catch (error) {
             
